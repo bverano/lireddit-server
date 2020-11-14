ce10385840905740206a493e875a80da8adb454e
Bruno Verano <bveranosk8@hotmail.com>
2020-11-13 21:47:54 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import mikroOrmConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { MyContext } from "./types";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroOrmConfig);
+  await orm.getMigrator().up(); //Automigrate when running server
+
+  const app = express();
+
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: "lax", //csrf
+        secure: __prod__, //cookie only works in https
+      },
+      saveUninitialized: false,
+      secret: "asdadadsdasdasdas",
+      resave: false,
+    })
+  );
+
+  //Set up Apollo Server
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
+  });
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(8080, () => {
+    console.log("Server running on port 8080");
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
+});
---
+++ src/types.ts
@@ -0,0 +1,11 @@
+import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
+import { Request, Response } from "express";
+
+interface SessionData {
+  userId: number;
+}
+export type MyContext = {
+  em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
+  req: Request & { session: Partial<SessionData> };
+  res: Response;
+};
